2dd611a3d5219d60d5d905a7824dcb638a64a2a0
Justin Trotter <trotter.justin@gmail.com>
2024-01-02 22:28:36 -0600

    strip out template shit

--- src/loading.rs
+++ src/loading.rs
@@ -1,7 +1,7 @@
 use crate::GameState;
 use bevy::prelude::*;
 use bevy_asset_loader::prelude::*;
-use bevy_kira_audio::AudioSource;
+// use bevy_kira_audio::AudioSource;
 
 pub struct LoadingPlugin;
 
@@ -13,7 +13,7 @@ impl Plugin for LoadingPlugin {
         app.add_loading_state(
             LoadingState::new(GameState::Loading)
                 .continue_to_state(GameState::Menu)
-                .load_collection::<AudioAssets>()
+                // .load_collection::<AudioAssets>()
                 .load_collection::<TextureAssets>(),
         );
     }
@@ -22,11 +22,11 @@ impl Plugin for LoadingPlugin {
 // the following asset collections will be loaded during the State `GameState::Loading`
 // when done loading, they will be inserted as resources (see <https://github.com/NiklasEi/bevy_asset_loader>)
 
-#[derive(AssetCollection, Resource)]
-pub struct AudioAssets {
-    #[asset(path = "audio/flying.ogg")]
-    pub flying: Handle<AudioSource>,
-}
+// #[derive(AssetCollection, Resource)]
+// pub struct AudioAssets {
+//     #[asset(path = "audio/flying.ogg")]
+//     pub flying: Handle<AudioSource>,
+// }
 
 #[derive(AssetCollection, Resource)]
 pub struct TextureAssets {
